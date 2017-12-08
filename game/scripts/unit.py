9ebad2a79227ff3a379d94dba8f59760756eab68
loul213 <canard.meteo@free.fr>
2017-12-08 14:16:23 +0100

    creation des stat des uniter

--- game/scripts/unit.py
+++ game/scripts/unit.py
@@ -1,23 +1,9 @@
-"""
-information:
-	le point(0,0) de la carte est le point en haut a droite
-	la coordonnée x est la coordonée de largeur
-	la coordonnée y est la coordonée de longueur
-
-
-donnée manquente:
-"""
-
 import scripts.utils as utils
 
-def generate_map(largueur_carte,longueur_carte):
-	"""renvoie une carte pleine d'eau (a finir)"""
-	carte=[["mer" for i in range(largueur_carte)] for j in range(longueur_carte)]
-	return(carte)
 
 class Unite:
 	def __init__(self):
-		self.type = "mer"
+		self.type = "sea"
 		self.point_vie = 0
 		self.point_deplacement = 0
 		self.porté_d_attaque = 0
@@ -26,6 +12,7 @@ class Unite:
 		self.zone_vision_mer = 0
 		self.zone_vision_terre = 0
 		self.zone_vision_air = 0
+		self.zone_vision_sous_marine = 0
 
 		self.position = utils.Position([0,0])
 
@@ -53,3 +40,99 @@ class Unite:
                 """fonction qui calcule les degats"""
                 ennemi.point_vie -= self.point_attaque
 
+
+class UnitesTerrestre(Unite):
+        def __init__(self):
+                Unite.__init__(self)
+                self.zone_vision_mer = 3
+		self.zone_vision_terre = 2
+		self.zone_vision_air = 2
+		self.zone_vision_sous_marine = 0
+
+class UnitesMaritime(Unite):
+        def __init__(self):
+                Unite.__init__(self)
+                self.zone_vision_mer = 3
+		self.zone_vision_terre = 2
+		self.zone_vision_air = 4
+		self.zone_vision_sous_marine = 1
+
+class UnitesSousMarine(Unite):
+        def __init__(self):
+                Unite.__init__(self)
+                self.zone_vision_mer = 2
+		self.zone_vision_terre = 1
+		self.zone_vision_air = 0
+		self.zone_vision_sous_marine = 3
+
+class UnitesAeriennes(Unite):
+        def __init__(self):
+                Unite.__init__(self)
+                self.zone_vision_mer = 5
+		self.zone_vision_terre = 3
+		self.zone_vision_air = 5
+		self.zone_vision_sous_marine = 0
+
+class InfanterieLourde(UnitesTerrestre):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 1
+		self.point_deplacement = 2
+		self.porté_d_attaque = 1
+		self.point_attaque = 1
+
+class Tanks(UnitesTerrestre):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 2
+		self.point_deplacement = 2
+		self.porté_d_attaque = 2
+		self.point_attaque = 2
+
+class Destroyes(UnitesMaritime):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 2
+		self.point_deplacement = 5
+		self.porté_d_attaque = 2
+		self.point_attaque = 2
+
+class Croiseurs(UnitesMaritime):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 3
+		self.point_deplacement = 3
+		self.porté_d_attaque = 4
+		self.point_attaque = 3
+
+class Cuirasses(UnitesMaritime):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 5
+		self.point_deplacement = 2
+		self.porté_d_attaque = 5
+		self.point_attaque = 4
+
+class PorteAvions(UnitesMaritime):
+        def __init__(self):
+                UnitesTerrestre.__init__(self)
+                self.point_vie = 5
+		self.point_deplacement = 3
+		self.porté_d_attaque = 2
+		self.point_attaque = 1
+
+class SousMarins(UnitesSousMarine):
+        def __init__(self):
+                UnitesSousMarine.__init__(self)
+                self.point_vie = 3
+		self.point_deplacement = 2
+		self.porté_d_attaque = 2
+		self.point_attaque = 3
+
+class Avions(UnitesAeriennes):
+        def __init__(self):
+                UnitesAeriennes.__init__(self)
+                self.point_vie = 1
+		self.point_deplacement = 10
+		self.porté_d_attaque = 1
+		self.point_attaque = 2

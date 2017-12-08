cccae4221334447522cae24c04e2176d532e272e
loul213 <canard.meteo@free.fr>
2017-12-08 14:59:27 +0100

    comande terminer

--- game/scripts/gameHandler.py
+++ game/scripts/gameHandler.py
@@ -1,10 +1,16 @@
-# Ce sentiment... De paix, et d'amour, si pur.... Est-il possiblede vivre avec lui?
-# Les conseils de cedric villani:
-#	Etre toujours en mouvement
-#	Se laisser guider par sa chance
+"""
+information:
+	le point(0,0) de la carte est le point en haut a droite
+	la coordonnée x est la coordonée de largeur
+	la coordonnée y est la coordonée de longueur
+
+
+donnée manquente:
+"""
 
 import pdb
 import scripts.utils as utils
+import scripts.unit as unit
 
 class Game:
 	"""A class designed to handle the behavior of the game, independently from events or graphical interface"""
@@ -13,6 +19,8 @@ class Game:
 
 		self.map = [[]]
 		self.loadMapFromFile(fileName)
+
+		self.tour = 1
 
 	def loadMapFromFile(self, filePath):
 		f = open(filePath, 'r')
@@ -34,3 +42,23 @@ class Game:
 
 	def updateTerrain():
 		print()
+
+	def generate_map(largueur_carte,longueur_carte):
+                """renvoie une carte pleine d'eau (a finir)"""
+                carte=[["sea" for i in range(largueur_carte)] for j in range(longueur_carte)]
+                return(carte)
+        
+        def tourSuivant(self):
+                self.tour += 1
+
+class Player:
+        def __init__(self):
+                self.nom = "random"
+                self.point = 0
+                self.unite = []
+
+        def calculPoint(self):
+                self.point = 0
+                for bateau in self.unite:
+                        self.point += bateau.point_vie
+                
